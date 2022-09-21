f40e229308d44e034effc1febacade8754f7be2a
Louis <louis.james@holisticai.com>
2022-09-21 15:59:53 +0100

    latest

--- steal_deploy_internal.sh
+++ steal_deploy_internal.sh
@@ -1,5 +1,6 @@
 echo "Stealing latest documentation build from holisticai directory..."
-rsync -r ../WITH_MITIGATION/holisticai/docs/_build/html/* .;
+rsync -r --delete /home/babe/src/holistic/hai-test/holisticai-test/docs_build/html/* .;
+
 echo "Pushing to github -> https://holisticaidocumentation.netlify.app/"
 git add -A .;
 git commit -am "latest";
