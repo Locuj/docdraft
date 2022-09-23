a14a8309d8cd90dd99c67116dedc233ce9042778
Louis <louis.james@holisticai.com>
2022-09-23 13:03:57 +0100

    update

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,16 @@
 # backup this script
 mv deploy.sh ./.git/deploy.sh
+
 # clean
 git rm -rf .
 git clean -fxd
+
 # copy new files
 cp -r ~/src/holistic/hai-test/holisticai-test/docs/_build/html/* .
+
 # bring back this script
 mv ./.git/deploy.sh .
+
 # add new files
 git add -A .
 git commit -am "update"
